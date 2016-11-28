2211c610dc81b0d4b86b19fb08d4bbdc81eeb7da
Wasupon <wasupon.wp@gmail.com>
2016-11-28 17:00:57 +0700

    bot commit

--- bot.php
+++ bot.php
@@ -86,7 +86,7 @@ if (!is_null($events['events'])) {
 			    "latitude": 13.8558361,
 			    "longitude": 100.5635089
 
-			]
+			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
