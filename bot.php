e078168ef6287cd06e0c44607238730f616fb6f5
Wasupon <wasupon.wp@gmail.com>
2016-11-28 11:15:40 +0700

    bot commit

--- bot.php
+++ bot.php
@@ -40,15 +40,15 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-			switch ($result) {
-				case 'สวัสดี':
-						echo "เออ ไหว้พระเถอะลูก ! \r\n";
-					break;
+			// switch ($result) {
+			// 	case 'สวัสดี':
+			// 			echo "เออ ไหว้พระเถอะลูก ! \r\n";
+			// 		break;
 				
-				default:
-						echo "อะไรเนี่ย ! ฉันยังฉลาดไม่พอ แต่ฉลาดกว่าคุณ\r\n";
-					break;
-			}
+			// 	default:
+			// 			echo "อะไรเนี่ย ! ฉันยังฉลาดไม่พอ แต่ฉลาดกว่าคุณ\r\n";
+			// 		break;
+			// }
 
 			
 		}
